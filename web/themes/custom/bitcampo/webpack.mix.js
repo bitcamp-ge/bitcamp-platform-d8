713d382bdc53bab9aa4131adada15a65bb601814
otarza <otar.zakalashvili@gmail.com>
2020-10-11 01:04:53 +0400

    Installed Radix theme. Created sub-theme Bitcampo based on Radix; Configured Bitcampo and basic blocks.

---
+++ web/themes/custom/bitcampo/webpack.mix.js
@@ -0,0 +1,48 @@
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your application. See https://github.com/JeffreyWay/laravel-mix.
+ |
+ */
+const proxy = 'http://bitcamp.ge.docker.localhost:8000';
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Configuration
+ |--------------------------------------------------------------------------
+ */
+mix
+  .setPublicPath('assets')
+  .disableNotifications()
+  .options({
+    processCssUrls: false
+  });
+
+/*
+ |--------------------------------------------------------------------------
+ | Browsersync
+ |--------------------------------------------------------------------------
+ */
+mix.browserSync({
+  proxy: proxy,
+  files: ['assets/js/**/*.js', 'assets/css/**/*.css'],
+  stream: true,
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | SASS
+ |--------------------------------------------------------------------------
+ */
+mix.sass('src/sass/bitcampo.style.scss', 'css');
+
+/*
+ |--------------------------------------------------------------------------
+ | JS
+ |--------------------------------------------------------------------------
+ */
+mix.js('src/js/bitcampo.script.js', 'js');
