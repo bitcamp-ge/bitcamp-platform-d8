c2614be98725e56892442647d76d69a469fa2e51
Temuri Takalandze <takalandzet@gmail.com>
2021-02-03 18:51:16 +0400

    :rotating_light: #15 - Fix PHPCS errors.

--- web/modules/custom/bcmp_user/src/Controller/BitCampUserPageController.php
+++ web/modules/custom/bcmp_user/src/Controller/BitCampUserPageController.php
@@ -4,18 +4,15 @@ namespace Drupal\bcmp_user\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\user\UserDataInterface;
-use Drupal\user\UserInterface;
 use Drupal\user\UserStorageInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /**
  * Defines BitCampUserPageController class.
  */
 class BitCampUserPageController extends ControllerBase {
-  
+
   /**
    * The user storage.
    *
@@ -37,7 +34,6 @@ class BitCampUserPageController extends ControllerBase {
    */
   protected $logger;
 
-
   /**
    * Constructs a BitCampUserPageController object.
    *
@@ -49,45 +45,48 @@ class BitCampUserPageController extends ControllerBase {
    *   A logger instance.
    */
   public function __construct(
-		UserStorageInterface $user_storage, 
-		UserDataInterface $user_data, 
-		LoggerInterface $logger) {
-	    $this->userStorage = $user_storage;
-  	  $this->userData = $user_data;
-    	$this->logger = $logger;
+    UserStorageInterface $user_storage,
+    UserDataInterface $user_data,
+    LoggerInterface $logger
+  ) {
+    $this->userStorage = $user_storage;
+    $this->userData = $user_data;
+    $this->logger = $logger;
   }
 
   /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container) {
-    return new static($container
-      ->get('entity.manager')
-      ->getStorage('user'), $container
-      ->get('user.data'), $container
-      ->get('logger.factory')
-      ->get('user'));
+    return new static(
+      $container
+        ->get('entity.manager')
+        ->getStorage('user'), $container
+        ->get('user.data'), $container
+        ->get('logger.factory')
+        ->get('user')
+    );
   }
 
-
-	/**
-	 * Redirects user to Phase 1 profile page.
-	 *
+  /**
+   * Redirects user to Phase 1 profile page.
+   *
    * This controller assumes that it is only invoked for authenticated users.
    * This is enforced for the 'user.page' route with the '_user_is_logged_in'
-   * requirement. 
-	 * 
+   * requirement.
+   *
    * @return \Symfony\Component\HttpFoundation\RedirectResponse
    *   Returns a redirect to the profile of the currently logged in user.
    */
-	public function phase1Page(){
-		 return $this
-    	->redirect('profile.user_page.single',
-				 [
-					'user' => $this->currentUser()->id(),
-					'profile_type' => 'phase_1' 
-         ]
-			);
-	}
-}
+  public function phase1Page() {
+    return $this
+      ->redirect(
+        'profile.user_page.single',
+        [
+          'user' => $this->currentUser()->id(),
+          'profile_type' => 'phase_1',
+        ]
+      );
+  }
 
+}
